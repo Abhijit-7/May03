391c01c21e33e6f3679ddc0766aa1329fc7cbcb5
Abhijit-7 <abhijitpandao123@gmail.com>
2022-05-03 11:03:49 +0530

    done

---
+++ Sum.java
@@ -0,0 +1,10 @@
+ class Sum {
+
+   public static void main(String[] args) {
+        
+      int num1 = 15, num2 = 15, add;
+      add = num1 + num2;
+
+      System.out.println("Sum of these numbers: "+add);
+   }
+}
